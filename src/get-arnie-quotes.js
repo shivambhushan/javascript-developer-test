fc83b688df1fd941b487e83875859607af8fd8cb
shivam bhushan <shivam.bhushan@r2p.com>
2025-08-25 22:22:51 +1000

    implemented getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,28 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+// Helper function to fetch a quote from a single URL
+async function getQuote(url) {
+	try {
+		const response = await httpGet(url);
+		const responseBody = JSON.parse(response.body);
+		const message = responseBody.message || '';
+		const status = response.status;
+
+		const objectKey = (status === 200 ? 'Arnie Quote' : 'FAILURE');
+		const resultObj = {};
+		resultObj[objectKey] = message;
+		return resultObj;
+	} catch (error) {
+		return { 'FAILURE': error.message || 'Unexpected unknown Error' };
+	}
+}
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+	// Mapped each URL to the helper function promise and waiting for all to resolve/reject
+	const results = await Promise.all(urls.map((url) => getQuote(url)));
+	return results;
 };
 
 module.exports = {
-  getArnieQuotes,
+	getArnieQuotes,
 };
